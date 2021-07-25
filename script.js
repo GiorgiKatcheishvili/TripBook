e13e6e498fb5fdd147ec96691bc1983cf9295344
GiorgiKatcheishvili <85373500+GiorgiKatcheishvili@users.noreply.github.com>
2021-07-25 20:48:35 +0400

    one
    1

---
+++ script.js
@@ -0,0 +1,7 @@
+let navbar = document.getElementById('navbar');
+let togglebutton= document.getElementById('togglebutton');
+
+
+togglebutton.addEventListener('click', function() { 
+    navbar.classList.toggle('active')
+})  
